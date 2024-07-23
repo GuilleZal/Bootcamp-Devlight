0e222e2df318d33645e2561a7333c3d87ce7386c
GuilleZal <guillezal153@gmail.com>
2024-07-23 02:47:30 -0300

    correccion ejercicios

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -1,52 +1,5 @@
 ﻿using System;
-/*
-int Temperatura;
-int Respuesta;
-int Respuesta2;
-
-do
-{
-    Console.WriteLine("Ingrese la temperatura actual en grados Celcius");
-    Temperatura = int.Parse(Console.ReadLine());
-
-    switch (Temperatura)
-    {
-        case < 0:
-            Console.WriteLine("Hace mucho frío afuera, asegúrate de abrigarte bien.");
-            break;
-        case > 20:
-            Console.WriteLine("Hace calor afuera, no necesitas una chaqueta.");
-            break;
-        case <= 20:
-            Console.WriteLine("El clima está fresco, una chaqueta ligera sería perfecta.");
-            break;
-        default:
-            break;
-    }
-
-    Console.WriteLine("¿Desea conocer el pronóstico para los próximos cinco días?.");
-    Console.WriteLine("Ingrese 1 para Aceptar");
-    Console.WriteLine("Ingrese 0 para Salir");
-    Respuesta = int.Parse(Console.ReadLine());
-    if (Respuesta == 1)
-    {
-        Console.WriteLine("Pronostico para los siguientes 5 dias:");
-        Console.WriteLine("Lunes: 12° ");
-        Console.WriteLine("Martes: 22° ");
-        Console.WriteLine("Miercoles: 30° ");
-        Console.WriteLine("Jueves: 20° ");
-        Console.WriteLine("Viernes: 15° ");
-    }
-
-    Console.WriteLine("¿Desesa realizar otra consulta?");
-    Console.WriteLine("Ingrese 1 para Aceptar");
-    Console.WriteLine("Ingrese 0 para Salir");
-    Respuesta2 = int.Parse(Console.ReadLine());
-} while (Respuesta2 == 1);
-Console.WriteLine("Saliendo...");
-
-*/
-
+using System.ComponentModel;
 //Ejercicios Opcionales
 
 //Ejercicio 1: Dado un valor, devolver un mensaje que diga “El valor es mayor que 100”
@@ -59,9 +12,10 @@ if (numero > 100)
 
 //Ejercicio 2. Pedir un número entero por teclado y calcular si es par o impar.
 
+int numero;
 Console.WriteLine("Ingrese un numero");
 numero = int.Parse(Console.ReadLine());
-if ((numero mod 2) == 0)
+if ((numero % 2) == 0)
 {
     Console.WriteLine("El numero es par");
 }
@@ -69,18 +23,22 @@ else
 {
     Console.WriteLine("El numero es impar");
 }
-*/
+
 // Ejercicio 3: Teniendo un valor entero, verificar si se cumple o no que ese valor es el
 // doble de un impar. Por ejemplo, 14 cumple con esta condición.
+
 int numero;
-numero = 14;
-if (((numero / 2) Mod 2) == 0)
+numero = 16;
+if ( ((numero / 2) % 2) == 0)
 {
     Console.WriteLine("El valor no cumple la condicion");
 }else
 {
     Console.WriteLine("El valor cumple la condicion");
 }
+*/
+//Ejercicio 4: Dada un número del 1 al 10, devolver su “versión” en números romanos.
 
-
-
+int numero;
+numero = 14;
+Console.WriteLine(Version(numero));
