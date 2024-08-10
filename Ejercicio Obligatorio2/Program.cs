6806d532962c1e486a10e6daaff2ebd372f39202
GuilleZal <guillezal153@gmail.com>
2024-08-10 10:08:25 -0300

    nuevo proyecto

---
+++ Ejercicio Obligatorio2/Program.cs
@@ -0,0 +1,10 @@
+﻿int[,] Mes = new int[5, 7];
+int temperatura;
+string respuesta;
+
+Console.WriteLine("Elija una opcion:");
+Console.WriteLine("1) Cargar las temperaturas del mes ");
+Console.WriteLine("2) Utilizar temperaturas genericas");
+
+Console.WriteLine("Ingrese la temperatura");
+temperatura = int.Parse(Console.ReadLine());
